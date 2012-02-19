50c19f98805344b37cd05df7ccc0df40fd9dac16
Tomáš Jukin <tomas@jukin.cz>
2012-02-19 14:21:39 +0100

    Generated git repository statistics

--- generate_stats.sh
+++ generate_stats.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-STATS_SCRIPT_PATH="/Users/inza/Projects/Tools/gitstats/gitstats"
-OUTPUT_PATH="GADCPrague_stats"
+GITSTATS_SCRIPT_PATH="/Users/inza/Projects/Tools/gitstats/gitstats"
+GOURCE_SCRIPT_PATH="/Users/inza/Projects/Tools/scripts/run_gource.sh"
+STATS_OUTPUT_PATH="GADCPrague_stats"
+VIDEO_OUTPUT_PATH="GADCPrague_visualization"
 
 # Update Submodules
 git submodule update
 
+# Clean
+find . -type d -mindepth 1 -maxdepth 1 -exec bash -c "cd '{}' && rm -Rf ./$VIDEO_OUTPUT_PATH ./$STATS_OUTPUT_PATH" \;
+
 # Checkout master branches
-find . -type d -maxdepth 1 -exec bash -c "cd '{}' && git checkout master" \;
+find . -type d -mindepth 1 -maxdepth 1 -exec bash -c "cd '{}' && git checkout master" \;
 
 # Generate stats
-find . -type d -maxdepth 1 -exec bash -c "cd '{}' && $STATS_SCRIPT_PATH . ./$OUTPUT_PATH" \;
+find . -type d -mindepth 1 -maxdepth 1 -exec bash -c "cd '{}' && $GITSTATS_SCRIPT_PATH . ./$STATS_OUTPUT_PATH" \;
 
 # Add, Commit and Push generated stats
-#find . -type d -maxdepth 1 -exec bash -c "cd '{}' && git add -A && git commit -m 'Generated git stats' && git push origin master" \;
+find . -type d -maxdepth 1 -exec bash -c "cd '{}' && git add -A && git commit -m 'Generated git repository statistics' && git pull origin master && git push origin master" \;
+
+# Update Submodules
+git submodule update
+
+# Generate gource
+find . -type d -mindepth 1 -maxdepth 1 -exec bash -c "cd '{}' && $GOURCE_SCRIPT_PATH $VIDEO_OUTPUT_PATH" \;
+
+# Add, Commit and Push generated stats
+find . -type d -maxdepth 1 -exec bash -c "cd '{}' && git add -A && git commit -m 'Generated git repository visualization' && git pull origin master && git push origin master" \;
